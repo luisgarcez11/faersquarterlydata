466992993c3d0824ed4cfc265d13298ef2e9acf3
luisgarcez11 <luisgarcez1@mail.com>
2024-06-21 18:06:33 +0100

    examples efficient

--- data-raw/producs_fda.R
+++ data-raw/producs_fda.R
@@ -7,10 +7,10 @@ products_fda <- read.csv("data-raw/products_FDA.txt", sep = "\t") %>%
 usethis::use_data(products_fda)
 
 
-als_faers_data$demographics = als_faers_data$demographics[1:1000,]
-als_faers_data$drug = als_faers_data$drug[1:1000,]
-als_faers_data$indication = als_faers_data$indication[1:1000,]
-als_faers_data$outcome = als_faers_data$outcome[1:1000,]
-als_faers_data$reaction = als_faers_data$reaction[1:2000,]
-als_faers_data$therapy = als_faers_data$therapy[1:1000,]
+als_faers_data$demographics = als_faers_data$demographics[1:100,]
+als_faers_data$drug = als_faers_data$drug[1:100,]
+als_faers_data$indication = als_faers_data$indication[1:100,]
+als_faers_data$outcome = als_faers_data$outcome[1:100,]
+als_faers_data$reaction = als_faers_data$reaction[1:300,]
+als_faers_data$therapy = als_faers_data$therapy[1:100,]
 usethis::use_data(als_faers_data, overwrite = TRUE)
