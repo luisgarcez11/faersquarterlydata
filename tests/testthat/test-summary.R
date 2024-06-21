9d83f3e085852a975077ef213aa5270bf7128c65
luisgarcez11 <luisgarcez1@mail.com>
2024-06-21 21:59:39 +0100

    final

--- tests/testthat/test-summary.R
+++ tests/testthat/test-summary.R
@@ -1,6 +1,6 @@
 testthat::test_that("summary_faersdata", {
   
-  faers_ascii_data <- retrieve_faersascii(ascii_dir = "tests/testthat/resources/test_zip_ex_dir/ASCII")
+  faers_ascii_data <- retrieve_faersascii(ascii_dir = "resources/test_zip_ex_dir/ASCII")
   faers_ascii_data_unified <- unify_tabular_ascii(ascii_list = faers_ascii_data)
   testthat::expect_true(is.list(summary_faersdata(faers_ascii_data_unified)))
 })
